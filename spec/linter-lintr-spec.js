61cde30cb1e8177c215c39672b04ca05cc71b2cb
Landon Abney <Landonabney@gmail.com>
2016-09-16 10:48:35 -0700

    Add functional tests
    Add some tests that ensure the linter is actually doing something, instead of just installed.

--- spec/linter-lintr-spec.js
+++ spec/linter-lintr-spec.js
@@ -1,6 +1,12 @@
 'use babel';
 
+import { join } from 'path';
+
+const badPath = join(__dirname, 'fixtures', 'bad.R');
+
 describe('The lintr provider for Linter', () => {
+  const lint = require(join('..', 'lib', 'main.coffee')).provideLinter().lint;
+
   beforeEach(() =>
     waitsForPromise(() =>
       atom.packages.activatePackage('linter-lintr')
@@ -14,4 +20,20 @@ describe('The lintr provider for Linter', () => {
   it('should be an active package', () =>
     expect(atom.packages.isPackageActive('linter-lintr')).toBe(true)
   );
+
+  it('reports messages from lintr', () => {
+    waitsForPromise(() =>
+      atom.workspace.open(badPath).then(editor => lint(editor)).then((messages) => {
+        expect(messages.length).toBeGreaterThan(0);
+
+        // test only the first error
+        expect(messages[0].type).toBe('Warning');
+        expect(messages[0].severity).toBe('warning');
+        expect(messages[0].html).not.toBeDefined();
+        expect(messages[0].text).toBe('Use <-, not =, for assignment.');
+        expect(messages[0].filePath).toBe(badPath);
+        expect(messages[0].range).toEqual([[0, 4], [0, 5]]);
+      })
+    );
+  });
 });
