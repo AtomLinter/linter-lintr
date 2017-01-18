aba8c4095e1047452937301b5045201aded9c6e5
Landon Abney <Landonabney@gmail.com>
2017-01-18 11:24:46 -0800

    Fix lint issues

--- spec/linter-lintr-spec.js
+++ spec/linter-lintr-spec.js
@@ -9,16 +9,16 @@ describe('The lintr provider for Linter', () => {
 
   beforeEach(() =>
     waitsForPromise(() =>
-      atom.packages.activatePackage('linter-lintr')
-    )
+      atom.packages.activatePackage('linter-lintr'),
+    ),
   );
 
   it('should be in the packages list', () =>
-    expect(atom.packages.isPackageLoaded('linter-lintr')).toBe(true)
+    expect(atom.packages.isPackageLoaded('linter-lintr')).toBe(true),
   );
 
   it('should be an active package', () =>
-    expect(atom.packages.isPackageActive('linter-lintr')).toBe(true)
+    expect(atom.packages.isPackageActive('linter-lintr')).toBe(true),
   );
 
   it('reports messages from lintr', () => {
@@ -33,7 +33,7 @@ describe('The lintr provider for Linter', () => {
         expect(messages[0].text).toBe('Use <-, not =, for assignment.');
         expect(messages[0].filePath).toBe(badPath);
         expect(messages[0].range).toEqual([[0, 4], [0, 14]]);
-      })
+      }),
     );
   });
 });
