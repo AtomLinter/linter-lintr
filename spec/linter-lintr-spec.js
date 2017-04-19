27ee5b0f063a07b2f952ce684ee422dc4b107701
Landon Abney <Landonabney@gmail.com>
2017-04-19 15:03:10 -0700

    Utilize async/await
    Refactor the code to take advantage of `async`/`await` where possible,
    including bringing in `jasmine-fix` to allow the specs to be re-written
    in a similar manner.

--- spec/linter-lintr-spec.js
+++ spec/linter-lintr-spec.js
@@ -1,17 +1,14 @@
 'use babel';
 
 import { join } from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { beforeEach, it } from 'jasmine-fix';
 
 const badPath = join(__dirname, 'fixtures', 'bad.R');
+const lint = require('../lib/main.js').provideLinter().lint;
 
 describe('The lintr provider for Linter', () => {
-  const lint = require('../lib/main.js').provideLinter().lint;
-
-  beforeEach(() =>
-    waitsForPromise(() =>
-      atom.packages.activatePackage('linter-lintr'),
-    ),
-  );
+  beforeEach(async () => atom.packages.activatePackage('linter-lintr'));
 
   it('should be in the packages list', () =>
     expect(atom.packages.isPackageLoaded('linter-lintr')).toBe(true),
@@ -21,19 +18,17 @@ describe('The lintr provider for Linter', () => {
     expect(atom.packages.isPackageActive('linter-lintr')).toBe(true),
   );
 
-  it('reports messages from lintr', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(badPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBeGreaterThan(0);
+  it('reports messages from lintr', async () => {
+    const editor = await atom.workspace.open(badPath);
+    const messages = await lint(editor);
+    expect(messages.length).toBeGreaterThan(0);
 
-        // test only the first error
-        expect(messages[0].type).toBe('Warning');
-        expect(messages[0].severity).toBe('warning');
-        expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].text).toBe('Use <-, not =, for assignment.');
-        expect(messages[0].filePath).toBe(badPath);
-        expect(messages[0].range).toEqual([[0, 4], [0, 14]]);
-      }),
-    );
+    // test only the first error
+    expect(messages[0].type).toBe('Warning');
+    expect(messages[0].severity).toBe('warning');
+    expect(messages[0].html).not.toBeDefined();
+    expect(messages[0].text).toBe('Use <-, not =, for assignment.');
+    expect(messages[0].filePath).toBe(badPath);
+    expect(messages[0].range).toEqual([[0, 4], [0, 14]]);
   });
 });
